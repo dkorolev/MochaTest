fc7d7a29098b0eba375c21d98c0e2403ab9ceecc
Dima <dmitry.korolev@gmail.com>
2014-05-30 10:07:31 -0700

    Version bump, local mocha, indents.

--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ var assert = require('assert');
 var code = require('../primes.js');
 
 describe('nextPrime', function() {
-  it('should return the next prime number after 7', function() {
-    assert.equal(11, code.nextPrime(7));
-  });
+    it('should return the next prime number after 7', function() {
+        assert.equal(11, code.nextPrime(7));
+    });
 
-  it('confirms that zero and one are not prime numbers', function() {
-    assert.equal(2, code.nextPrime(0));
-    assert.equal(2, code.nextPrime(1));
-  });
+    it('confirms that zero and one are not prime numbers', function() {
+        assert.equal(2, code.nextPrime(0));
+        assert.equal(2, code.nextPrime(1));
+    });
 });
